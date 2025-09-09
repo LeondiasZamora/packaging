da3c3d5b7cfcaafda2dc1d560912d9d946431690
Leonidas Zamora <leonidas.zamora.gottwald@gmail.com>
2025-09-09 10:40:54 +0200

    i changed the video configurations a bit to make the video play from the start and i also fixed the .play() logic in the stimulus controller

--- app/javascript/controllers/video_controller.js
+++ app/javascript/controllers/video_controller.js
@@ -1,27 +1,48 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="video"
-export default class extends Controller {
-  static targets = [ "button", "frame"]
+// export default class extends Controller {
+//   static targets = [ "button", "frame"]
 
-  play(){
+//   play(){
+//     this.buttonTarget.style.display = "none";
+//     this.frameTarget.style.display = "block";
+//     this.frameTarget.setAttribute("autoplay", "true")
+//     this.frameTarget.setAttribute("controls", "true")
+//     this.frameTarget.muted = false;
+//     this.frameTarget.currentTime = 0;
+
+//     this.frameTarget.play().then(() => {
+//       if (this.frameTarget.requestFullscreen) {
+//         this.frameTarget.requestFullscreen()
+//       } else if (this.frameTarget.webkitEnterFullscreen) {
+//         this.frameTarget.webkitEnterFullscreen()
+//       }
+//     })
+//   }
+
+//   connect() {
+//     this.buttonTarget.addEventListener( "click", () => this.play() );
+//   }
+// }
+
+export default class extends Controller {
+  static targets = ["button", "frame"]
+
+  play() {
     this.buttonTarget.style.display = "none";
     this.frameTarget.style.display = "block";
-    this.frameTarget.setAttribute("autoplay", "true")
-    this.frameTarget.setAttribute("controls", "true")
+
+    this.frameTarget.controls = true;
     this.frameTarget.muted = false;
     this.frameTarget.currentTime = 0;
 
-    this.playerTarget.play().then(() => {
-      if (this.playerTarget.requestFullscreen) {
-        this.playerTarget.requestFullscreen()
-      } else if (this.playerTarget.webkitEnterFullscreen) {
-        this.playerTarget.webkitEnterFullscreen()
-      }
-    })
+    this.frameTarget.play().catch(err => {
+      console.log("Playback prevented:", err);
+    });
   }
 
   connect() {
-    this.buttonTarget.addEventListener( "click", () => this.play() );
+    this.buttonTarget.addEventListener("click", () => this.play());
   }
 }
